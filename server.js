77b67a0f560419d4e1eccf53d4fdd6aa2e2f6658
Mohammadsarairha <mohammadsarairha@gmail.com>
2022-02-21 23:58:37 +0200

    Add data from online API

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 'use strict';
 
 const express = require("express");
+
 const movies = require("./movieData/data.json");
+
+const dotenv = require("dotenv");
+
+const axios = require("axios");
+
+dotenv.config();
 
 const app = express();
 
-function movieData(title, poster_path, overview) {
+const APIKEY = process.env.APIKEY;
+const PORT = process.env.PORT;
+
+
+function movieData(id, title, poster_path, overview) {
+    this.id = id;
     this.title = title;
     this.poster_path = poster_path;
     this.overview = overview;
@@ -27,12 +39,38 @@ function favoriteHandler(request, response) {
     return response.send("Welcome to Favorite Page");
 };
 
+app.get('/trending', trendinPageHandler);
+
+function trendinPageHandler(request, response) {
+
+    let result = [];
+
+    axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${APIKEY}&language=en-US`)
+        .then(apiResponse => {
+            apiResponse.data.results.map(value => {
+                let trendMovie = new movieData(value.id, value.title, value.poster_path, value.overview);
+                result.push(trendMovie);
+            })
+            return response.status(200).json(result);
+        }).catch(error => {
+            errorHandler(error, request, response);
+        })
+};
+
+function errorHandler(error, request, response) {
+    const err = {
+        status: 500,
+        message: error
+    }
+    return response.status(500).send(err);
+}
+
 app.use("*", notFoundHandler);
 
 function notFoundHandler(request, response) {
     return response.status(404).send("Not Found");
 }
 
-app.listen(3000, () => {
-    console.log("Listen on 3000");
+app.listen(PORT, () => {
+    console.log(`Listen on ${PORT}`);
 });
